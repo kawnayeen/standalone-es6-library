cb39d05c9ac89b87283224d331cc31e63ffff298
Kamarul Kawnayeen <kawnayeen.08@gmail.com>
2020-05-21 17:23:15 +0600

    gulp task for bundle and building library added

--- hello-lib/gulpfile.js
+++ hello-lib/gulpfile.js
@@ -13,4 +13,16 @@
                 reporter: 'spec'
             }));
     });
+
+    gulp.task('bundle', async () => {
+        await browserify({
+            entries: ['./src/main/app.js'],
+            standalone: 'HelloModule'
+        }).transform(babelify.configure({presets: ["@babel/env"]}))
+            .bundle().on('error', err => console.log(err))
+            .pipe(source('hello-lib.js'))
+            .pipe(gulp.dest('./build'));
+    });
+
+    gulp.task('build', gulp.series('test', 'bundle'));
 }());
